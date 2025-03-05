cfcd92cf07d0b9bec627ae1649de5bb7047f0d25
Maik Switalski <maik@zauberzeug.com>
2025-03-05 13:35:36 +0100

    initial commit

---
+++ Maui_iOS_RefreshView/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Maui_iOS_RefreshView;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Maui_iOS_RefreshView/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿namespace Maui_iOS_RefreshView;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        BindingContext = this;
+        RefreshView.Command = new Command(Reload);
+    }
+
+    bool waitBeforeRefreshing;
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        if (waitBeforeRefreshing)
+            await Task.Delay(100);
+
+        waitBeforeRefreshing = !waitBeforeRefreshing;
+        RefreshView.IsRefreshing = true;
+    }
+
+    async void Reload()
+    {
+        await Task.Delay(2000);
+        Content.Clear();
+
+        Content.Children.Add(new Label { Text = "Some layout" });
+        Content.Children.Add(new Button {
+            Text = "Open Subpage",
+            Command = new Command(async () => await Navigation.PushAsync(new ContentPage() {
+                Content = new Label {
+                    Text = "Empty page"
+                }
+            }))
+        });
+
+        RefreshView.IsRefreshing = false;
+    }
+
+    void MenuItem_OnClicked(object sender, EventArgs e)
+    {
+        RefreshView.IsRefreshing = true;
+    }
+}
